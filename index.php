76e15fc2435502fb3bf9cccb0ac7ca1a54791cb5
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-10-25 20:47:01 +0200

    Add auto-injecting proper date

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-
 	echo 'Receiver: ' . htmlspecialchars($_GET["mail"]) . "\n";
 	$to      = $_GET["mail"];
-	$subject = "Użytkowniku 43941298, ureguluj rachunek w Allegro";
-	$message = file_get_contents('./content.html');;
+	$subject = "Użytkowniku 43941298, opłaty prowizyjne za zakupy Allegro";
+	// $message = file_get_contents('./content.php');
+	$message = include('./content.php');
 	$headers  = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-Type: text/html; charset=utf-8" . "\r\n";
 	$headers .= "From: Allegro <powiadomienia@allegro.pl>" . "\r\n" .
@@ -11,5 +11,5 @@
 	    "X-Mailer: PHP/" . phpversion();
 
 	mail($to, $subject, $message, $headers);
-	echo "Message has been sent \n"
+	echo "Message has been sent \n";
 ?>
